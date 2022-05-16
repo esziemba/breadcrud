a9e42746062ba4b8934d03470ba468fbc07d3be8
esziemba <97561230+esziemba@users.noreply.github.com>
2022-05-16 11:11:12 -0500

    fixed misspelled content

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -9,7 +9,7 @@ function Default(html) {
             <body>
                 <h1>HTML Rendered</h1>
                 <div className="container">
-                    {html.childeren} 
+                    {html.children} 
                 </div>
             </body>
         </html>
